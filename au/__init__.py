c2b4804bd4dde83e8abd910d4c935c5b22f52ea4
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-05-29 14:36:23 +0200

    async utils

---
+++ au/__init__.py
@@ -0,0 +1,19 @@
+"""AU - Asynchronous Utilities"""
+
+from au.base import (
+    async_compute,
+    ComputationHandle,
+    ComputationStore,
+    ComputationResult,
+
+    ComputationStatus,
+    SerializationFormat,
+    FileSystemStore,
+    ProcessBackend,
+    Middleware,
+    LoggingMiddleware,
+    MetricsMiddleware,
+    temporary_async_compute,
+    ThreadBackend,
+    
+)
